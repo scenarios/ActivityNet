2467eee0f5a43ae5d2429a07bd320404e1e2d547
Yizhou Zhou <zyz0205@hotmail.com>
2021-03-28 21:29:46 +0800

    split train list into 5 node, totally 50 sub-lists

---
+++ Crawler/Kinetics/crawl_train_node_1.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+python download.py \
+./data/kinetics-700_train_split1.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split1.json &
+python download.py \
+./data/kinetics-700_train_split2.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split2.json &
+python download.py \
+./data/kinetics-700_train_split3.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split3.json &
+python download.py \
+./data/kinetics-700_train_split4.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split4.json &
+python download.py \
+./data/kinetics-700_train_split5.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split5.json &
+python download.py \
+./data/kinetics-700_train_split6.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split6.json &
+python download.py \
+./data/kinetics-700_train_split7.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split7.json &
+python download.py \
+./data/kinetics-700_train_split8.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split8.json &
+python download.py \
+./data/kinetics-700_train_split9.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split9.json &
+python download.py \
+./data/kinetics-700_train_split10.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split10.json
---
+++ Crawler/Kinetics/crawl_train_node_5.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+python download.py \
+./data/kinetics-700_train_split41.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split41.json &
+python download.py \
+./data/kinetics-700_train_split42.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split42.json &
+python download.py \
+./data/kinetics-700_train_split43.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split43.json &
+python download.py \
+./data/kinetics-700_train_split44.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split44.json &
+python download.py \
+./data/kinetics-700_train_split45.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split45.json &
+python download.py \
+./data/kinetics-700_train_split46.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split46.json &
+python download.py \
+./data/kinetics-700_train_split47.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split47.json &
+python download.py \
+./data/kinetics-700_train_split48.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split48.json &
+python download.py \
+./data/kinetics-700_train_split49.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split49.json &
+python download.py \
+./data/kinetics-700_train_split50.csv \
+/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+-n 1 -s 256 -t buffer_tmp \
+--download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split50.json
