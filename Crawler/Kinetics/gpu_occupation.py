0f683564d9a30f180ab7771dbd5ecf93e5b9ea5b
Yizhou Zhou <zyz0205@hotmail.com>
2021-03-29 20:00:11 +0800

    add to gpu device

--- Crawler/Kinetics/gpu_occupation.py
+++ Crawler/Kinetics/gpu_occupation.py
@@ -2,10 +2,10 @@ import torch
 
 
 def main():
-
+    _device = torch.device('cuda')
+    _tensor_a = torch.randn(10000).to_device(_device)
+    _tensor_b = torch.randn(4096, 10000).to_device(_device)
     while True:
-        _tensor_a = torch.randn(10000)
-        _tensor_b = torch.randn(4096, 10000)
         torch.matmul(_tensor_b, _tensor_a)
 
 
