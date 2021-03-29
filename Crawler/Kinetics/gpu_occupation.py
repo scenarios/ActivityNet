a60a7b0f38aff67e4de602fdd17f00e462489f97
Yizhou Zhou <zyz0205@hotmail.com>
2021-03-29 21:11:46 +0800

    bug fix: tensor.to(device)

--- Crawler/Kinetics/gpu_occupation.py
+++ Crawler/Kinetics/gpu_occupation.py
@@ -5,8 +5,8 @@ def main():
     _device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using device:', _device)
 
-    _tensor_a = torch.randn(10000).to_device(_device)
-    _tensor_b = torch.randn(4096, 10000).to_device(_device)
+    _tensor_a = torch.randn(10000).to(_device)
+    _tensor_b = torch.randn(4096, 10000).to(_device)
 
     print(_tensor_a.get_device())
     print(_tensor_b.get_device())
