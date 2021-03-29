0b5deabac74e5e3172f7693d04bd11f3b2b5eb53
Yizhou Zhou <zyz0205@hotmail.com>
2021-03-29 20:47:03 +0800

    add to gpu device debug print point

--- Crawler/Kinetics/gpu_occupation.py
+++ Crawler/Kinetics/gpu_occupation.py
@@ -2,11 +2,19 @@ import torch
 
 
 def main():
-    _device = torch.device('cuda')
+    _device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', _device)
+
     _tensor_a = torch.randn(10000).to_device(_device)
     _tensor_b = torch.randn(4096, 10000).to_device(_device)
+
+    print(_tensor_a.get_device())
+    print(_tensor_b.get_device())
+
+    c = torch.matmul(_tensor_b, _tensor_a)
+    print(c.get_device())
     while True:
-        torch.matmul(_tensor_b, _tensor_a)
+        c = torch.matmul(_tensor_b, _tensor_a)
 
 
 if __name__ == '__main__':
