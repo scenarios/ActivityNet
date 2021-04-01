6ba470668c3d43c30c85240bedf3ca67c4c9c730
Yizhou Zhou <zyz0205@hotmail.com>
2021-04-01 20:09:32 +0800

    add crawl remainings

---
+++ Crawler/Kinetics/crawl_train_node_rest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python gpu_occupation.py &
+for i in {8,15,23,36,37,47,49}
+do
+  python download.py \
+  ./data/kinetics-700_train_split$i.csv \
+  /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020 \
+  -n 1 -s 256 -t buffer_tmp \
+  --download-report /mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700-2020-train_download-report_split$i.json &
+done
+wait
